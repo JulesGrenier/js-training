18196180ee19c8a2f3565c774d7cc6dd161edf23
JulesGrenier <grenier.jules@outlook.com>
2018-09-16 20:44:56 +0200

    advanced tests

--- exercises/circular.js
+++ exercises/circular.js
@@ -8,15 +8,15 @@
  */
 
 // Your code :
-const circular = {
-    circular: this
-};
-//* Begin of tests
-const assert = require('assert')
+const circular = {};
 
-assert.strictEqual(typeof circular, 'object')
-assert.strictEqual(circular.circular, circular)
-assert.strictEqual(circular.circular.circular, circular)
-assert.strictEqual(circular.circular.circular.circular, circular)
-assert.strictEqual(circular.circular.circular.circular.circular, circular)
+circular['circular'] = circular;
+//* Begin of tests
+const assert = require('assert');
+
+assert.strictEqual(typeof circular, 'object');
+assert.strictEqual(circular.circular, circular);
+assert.strictEqual(circular.circular.circular, circular);
+assert.strictEqual(circular.circular.circular.circular, circular);
+assert.strictEqual(circular.circular.circular.circular.circular, circular);
 // End of tests */
--- exercises/multiply-recursive.js
+++ exercises/multiply-recursive.js
@@ -10,22 +10,34 @@
  */
 
 // Your code :
-
+function multiply(num1, num2) {
+    if (num1 === 0 || num2 === 0) return 0;
+    if (num1 === num2) return 1;
+    if (num1 > 0 && num2 > 0) {
+        return num2 + multiply(num1 - 1, num2);
+    } else if (num1 < 0 && num2 < 0) {
+        return Math.abs(num2) + multiply(Math.abs(num1) - 1, Math.abs(num2));
+    } else if (num1 < 0 && !(num2 < 0)) {
+        return num1 + multiply(num1, num2 - 1);
+    } else if (num2 < 0 && !(num1 < 0)) {
+        return num2 + multiply(num1 - 1, num2);
+    }
+};
 //* Begin of tests
-const assert = require('assert')
+const assert = require('assert');
 
-assert.strictEqual(typeof multiply, 'function')
-assert.strictEqual(multiply.length, 2)
-assert.strictEqual(multiply.toString().includes('Math.imul'), false)
-assert.strictEqual(multiply.toString().includes('while'), false)
-assert.strictEqual(multiply.toString().includes('for'), false)
-assert.strictEqual(multiply.toString().includes('*'), false)
-assert.strictEqual(multiply.toString().includes('/'), false)
-assert.strictEqual(multiply(34, 78), 2652)
-assert.strictEqual(multiply(123, 0), 0)
-assert.strictEqual(multiply(0, -230), 0)
-assert.strictEqual(multiply(0, 0), 0)
-assert.strictEqual(multiply(123, -22), -2706)
-assert.strictEqual(multiply(-22, 123), -2706)
-assert.strictEqual(multiply(-22, -123), 2706)
+assert.strictEqual(typeof multiply, 'function');
+assert.strictEqual(multiply.length, 2);
+assert.strictEqual(multiply.toString().includes('Math.imul'), false);
+assert.strictEqual(multiply.toString().includes('while'), false);
+assert.strictEqual(multiply.toString().includes('for'), false);
+assert.strictEqual(multiply.toString().includes('*'), false);
+assert.strictEqual(multiply.toString().includes('/'), false);
+assert.strictEqual(multiply(34, 78), 2652);
+assert.strictEqual(multiply(123, 0), 0);
+assert.strictEqual(multiply(0, -230), 0);
+assert.strictEqual(multiply(0, 0), 0);
+assert.strictEqual(multiply(123, -22), -2706);
+assert.strictEqual(multiply(-22, 123), -2706);
+assert.strictEqual(multiply(-22, -123), 2706);
 // End of tests */
